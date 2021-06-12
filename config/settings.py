534709a32799769a1ab828330f7797377e3736e9
Johnny Jimenez <johnny.robert.jimenez@gmail.com>
2021-06-12 14:36:16 -0700

    fixed static

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/img'),
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
